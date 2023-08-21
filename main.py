818f12a3a010d3b13d17299b961d432ddcfc3a88
LitvinT <litvin.lara74mail.ru@gmail.com>
2023-08-21 18:06:50 +0300

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import imaplib
 import email
-import chardet
 import re
 import time
 
@@ -11,22 +10,22 @@ def process_emails():
     imap.select("INBOX")
 
     # Получение UID всех писем в ящике
-    result, data = imap.uid("search", None, "ALL")
+    result, data = imap.search(None, "ALL")
     email_uids = data[0].split()
 
     # Открытие файла для записи данных
-    with open("parsed_emails.txt", "a", encoding="utf-8") as output_file:
+    with open("parsed_emails6.txt", "a", encoding="utf-8") as output_file:
         # Загрузка уже обработанных записей из файла
         processed_data = set()
         try:
-            with open("parsed_emails.txt", "r", encoding="utf-8") as existing_data_file:
+            with open("parsed_emails6.txt", "r", encoding="utf-8") as existing_data_file:
                 processed_data = set(existing_data_file.readlines())
         except FileNotFoundError:
             pass
 
         # Итерация по письмам
         for email_uid in email_uids:
-            result, data = imap.uid("fetch", email_uid, "(RFC822)")
+            result, data = imap.fetch(email_uid, "(RFC822)")
             msg = email.message_from_bytes(data[0][1])
 
             if msg.is_multipart():
@@ -34,12 +33,12 @@ def process_emails():
             else:
                 raw = msg.get_payload(decode=True)
 
-            encoding = chardet.detect(raw)['encoding']
-            text = raw.decode(encoding)
+            encoding = msg.get_charset() or "cp1251"  # Используем кодировку cp1251
 
-            # Извлечение данных из текста письма
-            pattern = r'Зачисление ([\d.]+ BYN [\d/]+ [\d:]+) "([^"]+)" счет ([A-Z\d]+)'
-            matches = re.findall(pattern, text)
+            # Извлекаем данные с помощью регулярного выражения
+            pattern = r'Зачисление\s+(\d+\.\d{2}\s+BYN\s+\d{2}/\d{2}/\d{2}\s+\d{2}:\d{2})\s+"([^"]+)"\s+счет\s+([A-Z\d]+)'
+            matches = re.findall(pattern, raw.decode(encoding), re.IGNORECASE | re.MULTILINE)
+
             for match in matches:
                 data_to_save = f"Зачисление {match[0]} \"{match[1].strip()}\" счет {match[2]}\n"
                 if data_to_save not in processed_data:
@@ -53,4 +52,4 @@ def process_emails():
 # Главный цикл
 while True:
     process_emails()  # Обработка существующих и новых писем
-    time.sleep(60)   # Пауза в 60 секунд перед следующей проверкой
+    time.sleep(60)  # Пауза в 60 секунд перед следующей проверкой
